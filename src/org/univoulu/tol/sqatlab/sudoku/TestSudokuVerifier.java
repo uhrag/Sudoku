865e19392b2770c6d6ab29cea00538a443ac182f
somename <someemail>
2015-10-09 01:45:59 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 
 	SudokuVerifier sVerify;
-	String incorretSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	String incorrectSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	
 	public void init(){
@@ -16,10 +16,9 @@ public class TestSudokuVerifier {
 	
 	@Test
 	public void testSudokuOnlyPositiveDigitsRule1() {
-		String actual =  correctSudoku;
-		String expected = correctSudoku;
+		int actual =  sVerify.verify(incorrectSudoku);
 		
-		assertEquals(actual, expected);
+		assertEquals(-1, actual);
 	}
 	
 /*	public void testSudokuAllDigitsAppearOnceInSubgridRule2() {
