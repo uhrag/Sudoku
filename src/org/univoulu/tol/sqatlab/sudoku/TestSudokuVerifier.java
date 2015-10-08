d92d9592fbb53f3da5e1c3a4ef27d06898928f77
somename <someemail>
2015-10-09 00:52:07 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -10,9 +10,46 @@ public class TestSudokuVerifier {
 	public void testSolutionIsValid() {
 		SudokuVerifier sudokuverifier = new SudokuVerifier();
 		String actual =  "123456789";
+		String expected = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		assertEquals(actual, expected);
+	}
+	
+	public void testSudokuOnlyPositiveDigitsRule1() {
+		SudokuVerifier sudokuverifier = new SudokuVerifier();
+		String actual =  "123456789";
 		String expected = "123456789";
 		
 		
 		assertEquals(actual, expected);
 	}
+	
+	public void testSudokuAllDigitsAppearOnceInSubgridRule2() {
+		SudokuVerifier sudokuverifier = new SudokuVerifier();
+		String actual =  "123456789";
+		String expected = "123456789";
+		
+		
+		assertEquals(actual, expected);
+	}
+	
+	public void testSudokuRowsRule3() {
+		SudokuVerifier sudokuverifier = new SudokuVerifier();
+		String actual =  "123456789";
+		String expected = "123456789";
+		
+		
+		assertEquals(actual, expected);
+	}
+	
+	public void testSudokuColumnsRule4() {
+		SudokuVerifier sudokuverifier = new SudokuVerifier();
+		String actual =  "123456789";
+		String expected = "123456789";
+		
+		
+		assertEquals(actual, expected);
+	}
+	
+	
 }
