84a7e444003c4b8c863f85d192b5bc81aae1c440
somename <someemail>
2015-10-09 01:39:41 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,12 +3,16 @@ package org.univoulu.tol.sqatlab.sudoku;
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
+		
+		// returns -1 if the string has
 		
 		for(char c: candidateSolution.toCharArray()){
-			if(!Character.isDigit(c)) {
+			if(!Character.isDigit(c) || c == '-') {
 				return -1;
 			}
 		}
+		
+		return 0;
+	}
 
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -10,30 +10,19 @@ public class TestSudokuVerifier {
 	String incorretSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	
-	public void setup(){
+	public void init(){
 		sVerify = new SudokuVerifier();
 	}
 	
 	@Test
-	public void testSolutionIsValid() {
-		String actual =  correctSudoku;
-		String expected = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		
-		assertEquals(actual, expected);
-	}
-	
-	@Test
 	public void testSudokuOnlyPositiveDigitsRule1() {
-		SudokuVerifier sudokuverifier = new SudokuVerifier();
-		String actual =  "123456789";
-		String expected = "123456789";
-		
+		String actual =  correctSudoku;
+		String expected = correctSudoku;
 		
 		assertEquals(actual, expected);
 	}
 	
 /*	public void testSudokuAllDigitsAppearOnceInSubgridRule2() {
-		SudokuVerifier sudokuverifier = new SudokuVerifier();
 		String actual =  "123456789";
 		String expected = "123456789";
 		
@@ -42,7 +31,6 @@ public class TestSudokuVerifier {
 	}
 	
 	public void testSudokuRowsRule3() {
-		SudokuVerifier sudokuverifier = new SudokuVerifier();
 		String actual =  "123456789";
 		String expected = "123456789";
 		
@@ -51,7 +39,6 @@ public class TestSudokuVerifier {
 	}
 	
 	public void testSudokuColumnsRule4() {
-		SudokuVerifier sudokuverifier = new SudokuVerifier();
 		String actual =  "123456789";
 		String expected = "123456789";
 		
